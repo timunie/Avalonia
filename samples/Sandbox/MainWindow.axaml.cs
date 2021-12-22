ea6f6210096765077af155559efd661ca97aeb1f
Tim <tim.uniewski@gmail.com>
2021-12-22 16:00:16 +0100

    Testing...

--- samples/Sandbox/MainWindow.axaml.cs
+++ samples/Sandbox/MainWindow.axaml.cs
@@ -1,7 +1,10 @@
+using System;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
 using Avalonia.Win32.WinRT.Composition;
+using Sandbox.Model;
 
 namespace Sandbox
 {
@@ -11,6 +14,19 @@ namespace Sandbox
         {
             this.InitializeComponent();
             this.AttachDevTools();
+
+            AddHandler(DragDrop.DropEvent, DoDrop);
+        }
+
+        private void DoDrop(object sender, DragEventArgs e)
+        {
+            if(e.Data.GetFileNames() is { } files && DataContext is MainViewModel viewModel)
+            {
+                foreach (var file in files)
+                {
+                    viewModel.FileItems.Add(new FileItem(file));
+                }
+            }
         }
 
         private void InitializeComponent()
---
+++ samples/Sandbox/Model/FileItem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Avalonia.Media.Imaging;
+using Microsoft.WindowsAPICodePack.Shell;
+
+namespace Sandbox.Model
+{
+    internal class FileItem
+    {
+        internal FileItem (string fileName)
+        {
+            FileName = fileName;
+        }
+
+        internal string FileName { get; init; }
+
+        internal Bitmap Icon
+        {
+            get {
+            
+                var shellFile = ShellFile.FromFilePath(FileName);
+                var thumbNail = shellFile.Thumbnail.ExtraLargeBitmap;
+
+                using var ms = new MemoryStream();
+
+                thumbNail.Save(ms, ImageFormat.Png);
+
+                return new Bitmap(ms);
+            }
+        } 
+    }
+}
